27a1bc80b566553ac67ab81f05ab48cc4040b606
Tomas Fernandez Narice <tfernandeznarice@mail.austral.edu.ar>
2025-03-31 12:42:36 -0300

    hand-in

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = f'{first_name} {last_name}'
+    print(full_name.lower())
+    print(first_name.capitalize(), last_name.capitalize())
+    print(full_name.upper())
+    print("\t",full_name.lower())
+ada()
